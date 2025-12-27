0cfd9b62104dad637eae396ffc3d02e9b8856b2f
Opaline <mn167167@gmail.com>
2025-12-28 01:02:06 +0700

    fix image path for navbar logo base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/qinin-cakes-jambi/',
   plugins: [react()],
-  base : '/qinin-cakes-jambi/',
 })
